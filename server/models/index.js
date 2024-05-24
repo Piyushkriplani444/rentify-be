ac068d767684f78bfca1ca0d4566bad347b9c2fc
Piyushkriplani444 <piyushkriplani444@gmail.com>
2024-05-24 18:50:51 +0530

    changes required to use models

--- server/models/index.js
+++ server/models/index.js
@@ -1,43 +1,66 @@
-'use strict';
+/* eslint-disable global-require */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable import/no-dynamic-require */
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(`${__dirname}/../config/config.js`)[env];
+
 const db = {};
+const sequelizeObj = {};
+const databases = Object.keys(config.databases);
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+/** Add Databases* */
+for (let i = 0; i < databases.length; i += 1) {
+  const database = databases[i];
+  const dbPath = config.databases[database];
+  sequelizeObj[i] = new Sequelize(
+    dbPath.database,
+    dbPath.username,
+    dbPath.password,
+    dbPath
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
+const getAllFiles = (dirPath, arrayOfFiles1) => {
+  const files = fs.readdirSync(dirPath);
+
+  let arrayOfFiles = arrayOfFiles1 || [];
+
+  files.forEach((file) => {
+    if (fs.statSync(`${dirPath}/${file}`).isDirectory()) {
+      arrayOfFiles = getAllFiles(`${dirPath}/${file}`, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, "/", file));
+    }
+  });
+
+  return arrayOfFiles;
+};
+
+/** Add the Database Models* */
+getAllFiles(`${__dirname}/../../server`)
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+      file.slice(-9) === ".model.js"
+  )
+  .forEach((file) => {
+    const model = require(file)(sequelizeObj[0], Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-db.sequelize = sequelize;
+db.sequelize = sequelizeObj[0];
+db.sequelize1 = sequelizeObj[1];
 db.Sequelize = Sequelize;
-
 module.exports = db;
